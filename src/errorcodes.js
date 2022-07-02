e916a9ea1cae0d82ccd27ff7e2037e378b77f366
Razvan Filip <razvan.filip.70@gmail.com>
2022-07-02 10:54:40 +0300

    Error codes

---
+++ src/errorcodes.js
@@ -0,0 +1,6 @@
+const ErrorCodes = {
+    INVALID_INPUT_ARGS_NB: 1,
+    INVALID_INPUT_ARGS_NAN: 2
+};
+
+export default ErrorCodes;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { getNearestShops } from "./app.js";
+import errorCodes from "./errorcodes.js"
 
 // Get command arguments
 const args = process.argv.slice(2);
@@ -6,13 +7,13 @@ const args = process.argv.slice(2);
 // Check if we received enough arguments. Extra arguments will be ignored
 if (args.length < 3) {
   console.log("Invalid input: two arguments required");
-  process.exit(1);
+  process.exit(errorCodes.INVALID_INPUT_ARGS_NB);
 }
 
 // Check if we received numbers as arguments
 if (isNaN(args[1]) || isNaN(args[2])) {
   console.log("Invalid input: numbers expected");
-  process.exit(1);
+  process.exit(errorCodes.INVALID_INPUT_ARGS_NAN);
 }
 
 const position = { x: args[1], y: args[2] };
