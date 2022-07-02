bfb21f78ef48880868b34e1f39f19ea1a29824e4
Razvan Filip <razvan.filip.70@gmail.com>
2022-07-02 18:28:43 +0300

    Remove redundant argument check

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import errorCodes from "./errorcodes.js";
 // Get command arguments
 const args = process.argv.slice(2);
 
-// Check if we received enough arguments. Extra arguments will be ignored
-if (args.length < 3) {
-  console.log("Invalid input: two arguments required");
-  process.exit(errorCodes.INVALID_INPUT_ARGS_NB);
-}
-
 // Check if we received numbers as arguments
 if (isNaN(args[1]) || isNaN(args[2])) {
   console.log("Invalid input: numbers expected");
