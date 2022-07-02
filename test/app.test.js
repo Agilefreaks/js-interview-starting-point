21e54abaadb3236c7a27d4eb52b96cc25f880714
Razvan Filip <razvan.filip.70@gmail.com>
2022-07-02 18:30:05 +0300

    Add input check tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,20 @@
 import { getNearestShops } from '../src/app';
+import errorCodes from "../src/errorcodes.js";
 
 describe('App', () => {
-  it('should return an array when the input is valid', () => {
-    expect(Array.isArray(getNearestShops({
-      lat: 0,
-      lng: 0,
-    }))).toBe(true);
+  it('should return an array when the input is valid', async () => {
+    const f = await getNearestShops({x: 0, y: 0});
+    expect(Array.isArray(f)).toBe(true);
+  });
+
+  it('should return an array even if the input is invalid', async () => {
+    const f = await getNearestShops({x: 0, y: "abc"});
+    expect(Array.isArray(f)).toBe(true);
+  });
+
+  it('should return an array with the error code if the input is invalid', async () => {
+    const f = await getNearestShops({x: 0, y: "abc"});
+    expect(f.length == 1).toBe(true);
+    expect(f[0]).toBe(errorCodes.INVALID_INPUT_ARGS_NAN);
   });
 });
