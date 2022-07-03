93d2d3f7eb4c1bbaea28b82faa14d9a0c32ae22c
Razvan Filip <razvan.filip.70@gmail.com>
2022-07-03 11:21:00 +0300

    Update error codes test

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,6 @@
 import { getNearestShops } from '../src/app';
 import errorCodes from "../src/errorcodes.js";
+import { ERROR_TOKEN} from "../src/utils.js"
 
 describe('App', () => {
   it('should return an array when the input is valid', async () => {
@@ -14,7 +15,8 @@ describe('App', () => {
 
   it('should return an array with the error code if the input is invalid', async () => {
     const f = await getNearestShops({x: 0, y: "abc"});
-    expect(f.length == 1).toBe(true);
-    expect(f[0]).toBe(errorCodes.INVALID_INPUT_ARGS_NAN);
+    expect(f.length == 2).toBe(true);
+    expect(f[0]).toBe(ERROR_TOKEN);
+    expect(f[1]).toBe(errorCodes.INVALID_INPUT_ARGS_NAN);
   });
 });
