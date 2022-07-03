09124b12c01470fb487598a4ce6c2df5476ccb34
Razvan Filip <razvan.filip.70@gmail.com>
2022-07-03 21:06:02 +0300

    Chain the token fetch with the coffee shops list fetch

--- src/app.js
+++ src/app.js
@@ -11,12 +11,15 @@ const COFFEE_SHOPS_URL =
   "https://blue-bottle-api-test.herokuapp.com/v1/coffee_shops?";
 
 /**
- * Fetch the token from the API
- * 
- * @returns {String} token
+ * Fetch the coffee shops list
+ *
+ * @returns {Array[]} an array containing the json coffee shops list and the response code
  */
-async function fetchToken() {
-  return fetch(TOKEN_URL, {
+async function fetchCoffeeShops() {
+  let responseCode;
+
+  // Fetch the token
+  let response = await fetch(TOKEN_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -24,15 +27,33 @@ async function fetchToken() {
     },
   })
     .then((response) => response.json())
-    .then((data) => data.token)
-    .catch((error) => {
-      console.log("Error while fetching the token", error);
+    .then((data) => {
+      const token = data.token;
+
+      // Chain the token fetch with the coffee shops list fetch
+      return fetch(
+        COFFEE_SHOPS_URL +
+          new URLSearchParams({
+            token: token,
+          }),
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      ).then((response) => {
+        responseCode = response.status;
+        return response.json();
+      });
     });
+
+  return [response, responseCode];
 }
 
 /**
  * Get the 3 closest coffee shops relative to current position
- * 
+ *
  * @param {Object} position
  * @param {Number} position.x
  * @param {Number} position.y
@@ -46,29 +67,8 @@ export async function getNearestShops(position) {
     return errorResponse(errorCodes.INVALID_INPUT_ARGS_NAN);
   }
 
-  // Get the token
-  let token = await fetchToken();
-
-  // Get the coffee shops list
-  let coffeeShops;
-  let responseCode;
-  await fetch(
-    COFFEE_SHOPS_URL +
-      new URLSearchParams({
-        token: token,
-      }),
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    }
-  )
-    .then((response) => {
-      responseCode = response.status;
-      return response.json();
-    })
-    .then((json) => (coffeeShops = json));
+  // Get the list of coffee shops
+  const [coffeeShops, responseCode] = await fetchCoffeeShops();
 
   // Handle different response codes
   switch (responseCode) {
